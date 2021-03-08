706150b13361f8a8904eb372f8bf7a5b4fbc8924
FruitbatM <mar.pavlovic@gmail.com>
2021-03-08 21:27:41 +0000

    add back to top arrow functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,25 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+// Back to Top Arrow - code with small modification taken from: 
+https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+mybutton = document.getElementById("arrow_2top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
