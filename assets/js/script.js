83cf069a4826a84b5f39f1edcfed80b3d5021f13
FruitbatM <mar.pavlovic@gmail.com>
2021-03-08 21:39:30 +0000

    add javascript function for back to arrow functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ mybutton = document.getElementById("arrow_2top");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 25|| document.documentElement.scrollTop > 25) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
