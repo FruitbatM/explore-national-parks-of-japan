7fdd6a302a4dcb6607a32bbb770ff2af50a1da79
FruitbatM <mar.pavlovic@gmail.com>
2021-03-09 08:38:29 +0000

    fix logo height for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 mybutton = document.getElementById("arrow_2top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 25px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -23,7 +23,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top of the page
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
