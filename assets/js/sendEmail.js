ae32a9069caf7a655fae3ca46bf77945d5cc8204
FruitbatM <mar.pavlovic@gmail.com>
2021-03-16 09:48:30 +0000

    Add SweetAlert modal

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,19 +1,28 @@
 // Send email function (adapted from Code Institute walkthrough project)
 
 function sendMail(contactForm) {
-    emailjs.send("japan", "template_29fdtiv", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+    emailjs.send('japan', 'template_29fdtiv', {
+        'from_name': contactForm.fullname.value,
+        'from_email': contactForm.email.value,
+        'subject': contactForm.subject.value,
+        'message': contactForm.message.value
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
+        function() {
+          // Sweet Alert custom pop-up alert if success
+          Swal.fire(
+            'Thank you for getting in touch!',
+            'We will get back to you shortly.',
+            'success'
+          );
         },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+        // Sweet Alert custom pop-up alert if fail
+        function() {
+          Swal.fire(
+            'Ooops...something went wrong.',
+            'error'
+            );
+          });
 
     // Clear the form
     document.getElementById('contact-form').reset();
