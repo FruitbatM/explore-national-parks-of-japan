0ba88d7c4a02fd6ea5a669af40879dbd409d472c
FruitbatM <mar.pavlovic@gmail.com>
2021-03-28 20:51:42 +0000

    Add additional weather data + create function to convert epoch time to user-frindly date

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -23,6 +23,34 @@ function displayWeather(data) {
     let fahrenheit = Math.round(((parseFloat(data.main.temp))*1.8)+32); //Convert temperature from celsius to fahrenheit
 
 	document.getElementById('temp-description').innerHTML = data.weather[0].description;
-	document.getElementById('temp-value').innerHTML = celsius + '&deg;' + 'C';
-	document.getElementById('location').innerHTML = data.name;
+    document.getElementById('temp-value').innerHTML = celsius + '&deg;' + ' C';
+    document.getElementById('wind-speed').innerHTML = 'Wind: ' + data.wind.speed;
+    document.getElementById('sunrise').innerHTML = 'Sunrise: ' + data.sys.sunrise;
+    document.getElementById('sunset').innerHTML = 'Sunset: ' + data.sys.sunset;
+    document.getElementById('location').innerHTML = data.name;
 }
+
+
+// API returns EPOCH time, this function converts it to a user-friendly date
+const convertEpochToDate = (time) => {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let fullDate = new Date(time * 1000);
+  return `${daysOfWeek[fullDate.getDay()]} ${fullDate.getDate()}`;
+};
+
+
+// API returns time that is local to where the request originated
+// The time needs to be relative to the location searched
+// There is an offset to calculate to convert this
+const convertEpochToTime = (time, shift) => {
+  let fullDate = new Date((time + shift) * 1000);
+  return `${fullDate.getHours()}:${fullDate.getMinutes()}:${fullDate.getSeconds()}`;
+};
