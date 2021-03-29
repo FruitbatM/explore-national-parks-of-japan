07c6800566be1941702cb95b723aa49fa2afb777
FruitbatM <mar.pavlovic@gmail.com>
2021-03-29 10:17:59 +0000

    Upload weather icons

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,9 @@
 // OpenWeather API
 
-function getWeather( cityID ) {
+function getWeather(cityID) {
   const apiKey = 'b3e0a4fb9d29b25beb45fdf2cad771b0';
-  
- fetch('https://api.openweathermap.org/data/2.5/weather?id=' + 1863357+ '&appid=' + apiKey + '&units=metric')   
+ 
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + 1863357 + '&appid=' + apiKey + '&units=metric')   
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
       displayWeather(data);
@@ -17,19 +17,23 @@ window.onload = function() {
   getWeather( 1863357 );
 }
 
+
+
 // Display live weather to User Interface
 function displayWeather(data) {
 	let celsius = Math.round(parseFloat(data.main.temp));
     let fahrenheit = Math.round(((parseFloat(data.main.temp))*1.8)+32); //Convert temperature from celsius to fahrenheit
 
+    document.getElementById('weather-icon').innerHTML = data.weather[0].icon + '<img src="assets/images/icons.png"/>';
 	document.getElementById('temp-description').innerHTML = data.weather[0].description;
     document.getElementById('temp-value').innerHTML = celsius + '&deg;' + ' C';
-    document.getElementById('wind-speed').innerHTML = 'Wind: ' + data.wind.speed;
+    document.getElementById('wind-speed').innerHTML = 'Wind: ' + data.wind.speed + ' m/s';
     document.getElementById('sunrise').innerHTML = 'Sunrise: ' + data.sys.sunrise;
     document.getElementById('sunset').innerHTML = 'Sunset: ' + data.sys.sunset;
     document.getElementById('location').innerHTML = data.name;
 }
 
+`<img src="icons/${weather.iconId}.png"/>`;
 
 // API returns EPOCH time, this function converts it to a user-friendly date
 const convertEpochToDate = (time) => {
