d78d501370e5aab59460fb91e1ca8f872ff099ab
FruitbatM <mar.pavlovic@gmail.com>
2021-04-08 12:56:30 +0000

    Add tooltip functionality to map section

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,55 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+// Tooltip
+
+const infoIcon = document.querySelector('#infoIcon');
+const tooltip = document.querySelector('#tooltip');
+
+const popperInstance = Popper.createPopper(infoIcon, tooltip, {
+  placement: 'right-start',
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 8],
+      },
+    },
+  ],
+});
+
+function show() {
+  // Make the tooltip visible
+  tooltip.setAttribute('data-show', '');
+
+    // Enable the event listeners
+    popperInstance.setOptions({
+      modifiers: [{ name: 'eventListeners', enabled: true }],
+    });
+
+    // Update its position
+    popperInstance.update();
+  }
+
+function hide() {
+  // Hide the tooltip
+  tooltip.removeAttribute('data-show');
+
+  // Disable the event listeners
+  popperInstance.setOptions({
+    modifiers: [{ name: 'eventListeners', enabled: false }],
+  });
+}
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach(event => {
+  button.addEventListener(event, show);
+});
+
+hideEvents.forEach(event => {
+  button.addEventListener(event, hide);
+});
